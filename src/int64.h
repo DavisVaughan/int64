bd065b3aa1703b9a47c5015b7ac31c9907e41c8b
DavisVaughan <davis@rstudio.com>
2020-01-26 10:04:30 -0500

    Requiring 3.5 so won't need compat

--- src/int64.h
+++ src/int64.h
@@ -15,16 +15,4 @@
 #define INTEGER64(x) ((long long*) DATAPTR(x))
 #define INTEGER64_RO(x) ((const long long*) DATAPTR_RO(x))
 
-// --------------------------------------------------------------
-// Compatibility
-
-#if (R_VERSION < R_Version(3, 5, 0))
-  #define LOGICAL_RO(x) ((const int*) LOGICAL(x))
-  #define INTEGER_RO(x) ((const int*) INTEGER(x))
-  #define REAL_RO(x) ((const double*) REAL(x))
-  #define COMPLEX_RO(x) ((const Rcomplex*) COMPLEX(x))
-  #define STRING_PTR_RO(x) ((const SEXP*) STRING_PTR(x))
-  #define RAW_RO(x) ((const Rbyte*) RAW(x))
-#endif
-
 #endif
