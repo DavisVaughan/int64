2d7255239cb0e00f5423b549ed5128f2d94339d1
DavisVaughan <davis@rstudio.com>
2020-02-01 10:13:03 -0800

    Feeling more certain about `NA_INT64` now

--- src/int64.h
+++ src/int64.h
@@ -10,7 +10,12 @@
 #define INT64(x) ((long long*) DATAPTR(x))
 #define INT64_RO(x) ((const long long*) DATAPTR_RO(x))
 
-// TODO is this right?
+// - This seems to be what base R does.
+// - -2147483648 is the smallest integer, and 2147483647 is the largest, so they
+//   just use the smallest integer as NA_integer_.
+// - We do something similar, -9223372036854775808 is the smallest long long
+//   value and 9223372036854775807 is the largest. We just use the smallest as
+//   NA_INT64.
 #define NA_INT64 LONG_LONG_MIN
 
 SEXP int64_subset(SEXP x, SEXP subscript);
