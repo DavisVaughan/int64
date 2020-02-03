07277b7aea6a546c3c9f419331234bb3e5231971
DavisVaughan <davis@rstudio.com>
2020-02-03 17:59:45 -0500

    Fix method signature

--- R/as.R
+++ R/as.R
@@ -10,11 +10,11 @@ as_int64 <- function(x) {
 }
 
 #' @export
-as.double.int64 <- function(x) {
+as.double.int64 <- function(x, ...) {
   vec_cast(x, double())
 }
 
 #' @export
-as.integer.int64 <- function(x) {
+as.integer.int64 <- function(x, ...) {
   vec_cast(x, integer())
 }
