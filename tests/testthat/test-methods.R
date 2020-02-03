e1ef2fe4f5684bd20f7eb8fe6436843bcf18cacb
DavisVaughan <davis@rstudio.com>
2020-02-03 17:56:53 -0500

    Basic order test

--- tests/testthat/test-methods.R
+++ tests/testthat/test-methods.R
@@ -5,3 +5,11 @@ test_that("can locate NA values", {
   x <- as_int64(c(NA, 1))
   expect_equal(is.na(x), c(TRUE, FALSE))
 })
+
+# ------------------------------------------------------------------------------
+# vec_order()
+
+test_that("order is correct", {
+  x <- as_int64(c(-10, 0, -5, 1, -2, 3))
+  expect_equal(vec_order(x), c(1, 3, 5, 2, 4, 6))
+})
